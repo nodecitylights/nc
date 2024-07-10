009c2cf570e9c75c75f62d97ac26485c2bee5455
Samantha <contact@samanthanguyen.me>
2024-07-10 17:16:19 -0500

    cleanup: code formatting (#5)

--- packages/nc/tests/lines.spec.ts
+++ packages/nc/tests/lines.spec.ts
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest'
 import { lines } from '../src'
 
 test('Hello\\nWorld', () => {
-  let s = lines('Hello\nWorld')
+	let s = lines('Hello\nWorld')
 	expect(s.next().value).toBe('Hello')
-  expect(s.next().value).toBe('World')
-  expect(s.next().value).toBeUndefined()
+	expect(s.next().value).toBe('World')
+	expect(s.next().value).toBeUndefined()
 })
