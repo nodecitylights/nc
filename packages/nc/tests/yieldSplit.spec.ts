8eef15e6096d763a38573216afe76eef4bcf7248
Samantha Nguyen <contact@samanthanguyen.me>
2024-07-10 17:12:33 -0500

    cleanup: code formatting

--- packages/nc/tests/yieldSplit.spec.ts
+++ packages/nc/tests/yieldSplit.spec.ts
@@ -2,10 +2,10 @@ import { expect, test } from 'vitest'
 import { yieldSplit } from '../src'
 
 test('yieldSplit()', () => {
-  let s = yieldSplit('hello,world,banana,apple', ',')
+	let s = yieldSplit('hello,world,banana,apple', ',')
 	expect(s.next().value).toBe('hello')
-  expect(s.next().value).toBe('world')
-  expect(s.next().value).toBe('banana')
-  expect(s.next().value).toBe('apple')
-  expect(s.next().value).toBeUndefined()
+	expect(s.next().value).toBe('world')
+	expect(s.next().value).toBe('banana')
+	expect(s.next().value).toBe('apple')
+	expect(s.next().value).toBeUndefined()
 })
--- packages/nc/tests/yieldSplitBy.spec.ts
+++ packages/nc/tests/yieldSplitBy.spec.ts
@@ -4,10 +4,10 @@ import { yieldSplitBy } from '../src'
 const isNumber = (c: string): boolean => '123456789'.includes(c)
 
 test('yieldSplitBy()', () => {
-  let s = yieldSplitBy('hello1world2banana3apple', isNumber)
+	let s = yieldSplitBy('hello1world2banana3apple', isNumber)
 	expect(s.next().value).toBe('hello')
-  expect(s.next().value).toBe('world')
-  expect(s.next().value).toBe('banana')
-  expect(s.next().value).toBe('apple')
-  expect(s.next().value).toBeUndefined()
+	expect(s.next().value).toBe('world')
+	expect(s.next().value).toBe('banana')
+	expect(s.next().value).toBe('apple')
+	expect(s.next().value).toBeUndefined()
 })
